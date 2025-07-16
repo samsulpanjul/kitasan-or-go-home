6a03a5ead11bac19ae73487e9da8179e2ff110aa
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-07-17 05:34:39 +0700

    KITASAN

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pymsgbox as msg
 
 pyautogui.useImageNotFoundException(False)
 
-CONFIDENCE=0.8
-KITASAN_IMG="ss.png"
+CONFIDENCE=0.9
+KITASAN_IMG="assets/kitasan.png"
 
 def switch_window(title):
   uma = gw.getWindowsWithTitle(title)
@@ -62,11 +62,9 @@ def register():
   time.sleep(1)
 
   view_btn = locate_all_image("assets/view_f_btn.png")
-  print(f"Ditemukan {len(view_btn)} tombol:")
-  for i, btn in enumerate(view_btn):
+  for _, btn in enumerate(view_btn):
     print("VIEW BTN CLICKED")
     pyautogui.click(btn)
-    # print(f"{i+1}. {btn}")
   
   time.sleep(1)
   switch_window("Umamusume")
@@ -96,10 +94,10 @@ def register():
   click("assets/ok_btn.png")
 
   for _ in range(10):
-    skip_icon_btn = pyautogui.locateCenterOnScreen("assets/skip_icon_btn.png", confidence=CONFIDENCE, minSearchTime=1)
+    skip_icon_btn = pyautogui.locateCenterOnScreen("assets/skip_icon_btn.png", confidence=CONFIDENCE, minSearchTime=5)
     if skip_icon_btn:
       pyautogui.tripleClick(skip_icon_btn, interval=0.1)
-      time.sleep(0.5)
+    time.sleep(0.5)
 
   for _ in range(2):
     click("assets/close_btn.png")
@@ -114,7 +112,7 @@ def claim_mail():
 
   for _ in range(2):
     click("assets/close_btn.png")
-    time.sleep(0.3)
+    time.sleep(0.5)
 
 def select_banner():
   print("\nSELECT BANNER!\n")
@@ -122,11 +120,17 @@ def select_banner():
 
   if current_banner:
     pyautogui.moveTo(current_banner, duration=0.15)
+    pyautogui.tripleClick(current_banner, interval=0.7)
+    print("CURRENT BANNER CLICKED")
+  else:
+    pyautogui.moveTo(817,659, duration=0.15)
     pyautogui.click()
+    print("MOVE MOUSE MANUALLY USING COORD")
 
 def gacha(pulls: int = 5, kitasan_copy = 0, how_many_kitasan = 3):
   print("\nGACHA!\n")
   click("assets/gacha_menu.png")
+  time.sleep(0.5)
 
   select_banner()
 
@@ -144,11 +148,11 @@ def gacha(pulls: int = 5, kitasan_copy = 0, how_many_kitasan = 3):
   kitasan_found = locate_all_image(KITASAN_IMG)
   if kitasan_found:
     kitasan_copy = kitasan_copy + len(kitasan_found)
-    print(f"Got {kitasan_copy} Kitasan Black")
+    print(f"Got {kitasan_copy} Kitasan")
 
-  if kitasan_copy >= how_many_kitasan:
+  if kitasan_copy >= int(how_many_kitasan):
     print(f"Done, already got {kitasan_copy} Kitasan.")
-    return True
+    return kitasan_copy
 
   for _ in range(pulls):
     click("assets/scout_again_btn.png")
@@ -159,15 +163,15 @@ def gacha(pulls: int = 5, kitasan_copy = 0, how_many_kitasan = 3):
         pyautogui.tripleClick(skip_icon_btn, interval=0.1)
         time.sleep(0.2)
       
-      kitasan_found = locate_all_image(KITASAN_IMG)
-      if kitasan_found:
-        kitasan_copy = kitasan_copy + len(kitasan_found)
-        print(f"Got {kitasan_copy} Kitasan Black")
+    kitasan_found = locate_all_image(KITASAN_IMG)
+    if kitasan_found:
+      kitasan_copy = kitasan_copy + len(kitasan_found)
+      print(f"Got {kitasan_copy} Kitasan")
     
     time.sleep(0.5)
     if kitasan_copy >= how_many_kitasan:
       print(f"Done, already got {kitasan_copy} Kitasan.")
-      return True
+      return kitasan_copy
 
   # click("assets/back_btn.png")
 
@@ -177,7 +181,7 @@ def gacha(pulls: int = 5, kitasan_copy = 0, how_many_kitasan = 3):
   # click("assets/close_btn.png")
 
   time.sleep(0.5)
-  return False
+  return kitasan_copy
 
 def delete_data():
   print("\nREROLL\n")
@@ -196,27 +200,34 @@ def delete_data():
   click("assets/close_btn.png")
 
 def main():
+  pulls = msg.prompt("How many pulls?")
   how_many_kitasan = msg.prompt("How many kitasan do you want?")
-  kitasan = 0
 
-  if how_many_kitasan is not None:
+  if not pulls or not pulls.isdigit(): msg.alert("Input number.")
+
+  if how_many_kitasan is not None and how_many_kitasan.isdigit():
     switch_window("Umamusume")
     
     while True:
+      kitasan = 0
       register()
 
       time.sleep(1.5)
 
       claim_mail()
 
-      result = gacha(5, kitasan, int(how_many_kitasan))
+      result = gacha(int(pulls) - 1, kitasan, int(how_many_kitasan))
 
-      if result:
-        msg.alert("Done, grats")
+      if result >= int(how_many_kitasan):
+        msg.alert(f"Done, grats got {result} Kitasan")
+        print(f"Kitasan: {result}")
         break
       else:
+        print("Didn't get kitasan, rerolling...")
         delete_data()
 
         time.sleep(5)
+  else:
+    msg.alert("Input number.")
 
 main()
